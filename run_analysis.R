be374025a5dd1b5d635ba34fd05cfbe17371a86c
Vince <vinny_21_2000@yahoo.com>
2019-05-10 01:07:02 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,16 +17,18 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 # download data to temp directory
-temp <- tempfile( fileext = ".zip")
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", temp)
-# extract file to current directory subfolder 'data'
-unzip(temp, exdir = "./data")
-unlink(temp)
+if(!file.exists("UCI HAR Dataset") ){
+  temp <- tempfile( fileext = ".zip")
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", temp)
+  # extract file to current directory subfolder 'data'
+  unzip(temp, exdir = ".")
+  unlink(temp)
+}
 
 # get file paths to read from folders
-cmnfiles <- list.files(path = "./data/UCI HAR Dataset", pattern = "*.txt", full.names = TRUE)
-trainfiles <- list.files(path = "./data/UCI HAR Dataset/train", pattern = "*.txt", full.names = TRUE)
-testfiles <- list.files(path = "./data/UCI HAR Dataset/test", pattern = "*.txt", full.names = TRUE)
+cmnfiles <- list.files(path = "./UCI HAR Dataset", pattern = "*.txt", full.names = TRUE)
+trainfiles <- list.files(path = "./UCI HAR Dataset/train", pattern = "*.txt", full.names = TRUE)
+testfiles <- list.files(path = "./UCI HAR Dataset/test", pattern = "*.txt", full.names = TRUE)
 
 # read files to list
 trainfile_list <- lapply(trainfiles, read.table, stringsAsFactors=FALSE )
@@ -34,9 +36,9 @@ testfile_list <- lapply(testfiles, read.table, stringsAsFactors=FALSE )
 label_list <- lapply(cmnfiles[1:2], read.table, stringsAsFactors=FALSE )
 
 # rename files in list
-names(trainfile_list) <- substring(trainfiles, 30)
-names(testfile_list) <- substring(testfiles, 29)
-names(label_list) <- substring(cmnfiles[1:2], 24)
+names(trainfile_list) <- substring(trainfiles, 25)
+names(testfile_list) <- substring(testfiles, 24)
+names(label_list) <- substring(cmnfiles[1:2], 19)
 
 # apply feature labels to data frames in train list
 colnames(trainfile_list$X_train.txt) <- label_list$features.txt[,2]
@@ -49,7 +51,11 @@ colnames(testfile_list$y_test.txt) <- 'activityID'
 colnames(testfile_list$subject_test.txt) <- 'subjectID'
 
 # load dplyr librar for data manipulations
-library(dplyr)
+if (!'dplyr' %in% installed.packages() ){
+  install.packages('dplyr', dependencies = TRUE)
+}
+
+library('dplyr')
 
 # merge datasets
 trainTest_meanStd_Activities <- bind_cols(trainfile_list) %>%
@@ -58,10 +64,12 @@ trainTest_meanStd_Activities <- bind_cols(trainfile_list) %>%
         # reorder dataset
         select(., c(1, length(.), 2:length(.)-1 ) ) %>%
         # remove columns whose labels do not include 'ID', 'mean()', or 'std()'
-         .[,ifelse(regexpr('*ID|mean[()]|*std[()]*', colnames(.) ) > 0 , TRUE, FALSE) ] 
+        .[,ifelse(regexpr('*ID|mean[()]|*std[()]*', colnames(.) ) > 0 , TRUE, FALSE) ] %>%
+        # summmarize dataset by activity ID, subject ID, and mean of each feature 
+        aggregate(., list( .$activityID, .$subjectID), mean) %>%
+        select(., c(-1,-2)) 
 
-# summmarize dataset by activity ID, subject ID, and mean of each feature 
-subjectActivity_Summary <- trainTest_meanStd_Activities %>%
-                  aggregate(., list( .$activityID, .$subjectID), mean) %>%
-                  select(., c(-1,-2))
+# write.table(trainTest_meanStd_Activities, file = "trainTest_meanStd_Activities.txt", row.names = FALSE, col.names = TRUE)
+
+
   
